3a85b33598773410659ca06e3395e3365052e4c5
Joshua Clement <jaclem12@gmail.com>
2022-01-25 20:55:09 -0500

    Kinda works...

--- js/app.js
+++ js/app.js
@@ -1,92 +1,88 @@
-const numbers = document.querySelectorAll('#num');
-const switches = document.querySelectorAll('#switches');
-const output = document.getElementById('output');
+const numbers = document.querySelectorAll('[data-number]');
+const operators = document.querySelectorAll('[data-operation]');
+const clearAll = document.querySelector('[data-clear]');
+const equalsBtn = document.querySelector('[data-equals]');
+let previousValue = document.querySelector('[data-previous]');
+let currentValue = document.querySelector('[data-current]');
+let holdNum = '';
+let firstNum = '';
+let secondNum = '';
+let operand = '';
 
-
-const add = function(nums){
-    const sum = nums.reduce((total, current) => {
-        return total + current;
-    }, 0);
-    return sum;
+const add = function(a, b){
+    return a + b;
 }
 
-const subtract = function(nums){
-    const sum = nums.reduce((total, current) => {
-        return current - total;
-    }, 0);
-    return sum;
+const subtract = function(a, b){
+    return a - b;
 }
 
-const multiply = function(nums){
-    const sum = nums.reduce((total, current) => {
-        return total * current;
-    }, 1);
-    return sum;
+const multiply = function(a, b){
+    return a * b;
 }
 
-const divide = function(nums){
-    const sum = nums.reduce((total, current) => {
-        return total / current;
-    });
-    return sum;
+const divide = function(a, b){
+    return a / b;
 }
 
-const operate = function(operator, ...nums){
+const calculate = function(){
+    a = Number(firstNum);
+    o = operand;
+    b = Number(secondNum);
+    let value;
 
-    // switch checks operator to see if it matches any of the value strings
-    // if it's a match it calls one of the functions that performs that calculation 
-    switch(operator){
-        case '+':
-            let added = add(nums);
-            return added;
-        case '-':
-            let subtracted = subtract(nums);
-            return subtracted;
-        case '*':
-            let multiplied = multiply(nums);
-            return multiplied;
-        case '/':
-            let divided = divide(nums);
-            return divided;
+    if(b != 0){
+        switch(o){
+            case '+':
+                value = add(a, b);
+                previousValue.innerHTML = value;
+                break;
+            case '-':
+                value = subtract(a, b);
+                previousValue.innerHTML = value;
+                break;
+            case '*':
+                value = multiply(a, b);
+                previousValue.innerHTML = value;
+                break;
+            case '/':
+                value = divide(a, b);
+                previousValue.innerHTML = value;
+                break;
+        }
     }
 }
 
-const getVal = function(){
-    let numberVal = Number(output.value);
-    return numberVal;
-}
+numbers.forEach(number => {
+    number.addEventListener('click', ()=> {
+        currentValue.innerHTML += number.innerHTML;
+        holdNum += number.innerHTML;
 
-const getOp = function(op){
-    let operatorVal = op;
-    return operatorVal;
-}
+        if(operand != ''){
+            secondNum += number.innerHTML;
+        }
 
-const arrayFunc = function(){
-    let value;
-    numbers.forEach(number => {
-        number.addEventListener('click', () =>{
-            value = number.innerHTML;
-            output.value += `${value}`;
-            getVal();
-        });
-    });  
-}
-arrayFunc();
-
-const operators = function(){
-    let value;
-    switches.forEach(operator => {
-        operator.addEventListener('click', () =>{
-            value = operator.innerHTML;
-            output.value += `${value}`;
-            getOp(value);
-        });
+        calculate();
     });
-}
-operators();
+});
 
-const evaluate = function(){
-    console.log(getVal());
-    console.log(getOp());
-}
-evaluate();
+
+operators.forEach(operator => {
+    operator.addEventListener('click', ()=> {
+        currentValue.innerHTML += operator.innerHTML;  
+        firstNum = holdNum;
+        operand = operator.innerHTML;
+        holdNum = '';
+        calculate();
+    });
+});
+
+clearAll.addEventListener('click', ()=> {
+    previousValue.innerHTML = '';
+    currentValue.innerHTML = '';
+});
+
+equalsBtn.addEventListener('click', ()=> {
+    currentValue.innerHTML = previousValue.innerHTML;
+    previousValue.innerHTML = '';
+})
