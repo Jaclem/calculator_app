be120eabc7bd37fce685632822d6d5b8c30b5e41
Joshua Clement <jaclem12@gmail.com>
2022-01-27 13:12:39 -0500

    Progress I think

--- js/app.js
+++ js/app.js
@@ -4,76 +4,32 @@ const clearAll = document.querySelector('[data-clear]');
 const equalsBtn = document.querySelector('[data-equals]');
 let previousValue = document.querySelector('[data-previous]');
 let currentValue = document.querySelector('[data-current]');
-let holdNum = '';
 let firstNum = '';
 let secondNum = '';
 let operand = '';
-
-const add = function(a, b){
-    return a + b;
-}
-
-const subtract = function(a, b){
-    return a - b;
-}
-
-const multiply = function(a, b){
-    return a * b;
-}
-
-const divide = function(a, b){
-    return a / b;
-}
-
-const calculate = function(){
-    a = Number(firstNum);
-    o = operand;
-    b = Number(secondNum);
-    let value;
-
-    if(b != 0){
-        switch(o){
-            case '+':
-                value = add(a, b);
-                previousValue.innerHTML = value;
-                break;
-            case '-':
-                value = subtract(a, b);
-                previousValue.innerHTML = value;
-                break;
-            case '*':
-                value = multiply(a, b);
-                previousValue.innerHTML = value;
-                break;
-            case '/':
-                value = divide(a, b);
-                previousValue.innerHTML = value;
-                break;
-        }
-    }
-}
+let content = '';
 
 numbers.forEach(number => {
+
     number.addEventListener('click', ()=> {
-        currentValue.innerHTML += number.innerHTML;
-        holdNum += number.innerHTML;
-
-        if(operand != ''){
-            secondNum += number.innerHTML;
+        currentValue.innerHTML += number.textContent;
+        firstNum += number.textContent;
+        
+        if(operand == ''){
+            getFirstVal(firstNum); 
         }
-
-        calculate();
+        else if(operand != ''){
+            secondNum += number.textContent;
+            getSecondVal(secondNum);
+        }  
     });
 });
 
 
 operators.forEach(operator => {
     operator.addEventListener('click', ()=> {
-        currentValue.innerHTML += operator.innerHTML;  
-        firstNum = holdNum;
+        currentValue.innerHTML += operator.innerHTML;
         operand = operator.innerHTML;
-        holdNum = '';
-        calculate();
     });
 });
 
@@ -83,6 +39,29 @@ clearAll.addEventListener('click', ()=> {
 });
 
 equalsBtn.addEventListener('click', ()=> {
-    currentValue.innerHTML = previousValue.innerHTML;
+    firstNum = currentValue.innerHTML;
     previousValue.innerHTML = '';
-})
+});
+
+const getFirstVal = function(...args){
+    console.log(args);
+}
+
+const getSecondVal = function(...args){
+    console.log(args);
+}
+
+
+// const calculate = function(){
+    
+//     switch(){
+//         case '+':
+//             return a + b;
+//         case '-':
+//             return a - b;
+//         case '*':
+//             return a * b;
+//         case '/':
+//             return a / b;
+//     }
+// }
